7c9cee8ee5b0791712e4704d74c9c8833b205de3
Leithen <leithen113@gmail.com>
2022-02-02 13:55:26 -0700

    Dynamically rending components based on logged in status

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 const Header = () => {
+  function logout(evt) {
+    evt.preventDefault();
+    Auth.logout();
+  }
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -10,8 +15,19 @@ const Header = () => {
         </Link>
 
         <nav className="text-center">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import FriendList from '../components/FriendList';
+import Auth from '../utils/auth';
 
 const Profile = () => {
   const { username: userParam } = useParams();
-  const { loading, data } = useQuery(QUERY_USER, {
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
-  const user = data?.user || {};
+  const user = data?.me || data?.user || {};
+
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Redirect to="/profile" />;
+  }
+
   if (loading) {
     return <div>Loading...</div>;
   }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h4>
+    )
+  }
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {user.username}'s profile.
-        </h2>
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2> 
       </div>
 
       <div className="flex-row justify-space-between mb-3">
